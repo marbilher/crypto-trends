6977f10095a85f1464a3f2635107f34d3e98632b
Marlon B Hernandez <marlonbilbaohernandez@gmail.com>
2018-09-17 09:17:39 -0700

    prep for static demo

---
+++ src/MainPage.js
@@ -0,0 +1,154 @@
+import React from "react";
+// import * as users from "./services/users.service"
+import './App.css';
+import ScrapeTrendsHighChart from './ScrapeTrends.js'
+
+ class MainPage extends React.Component {
+  
+ 	constructor(props) {
+	    super(props);
+	
+	    this.state = {
+	      placeholder: 'ok'
+	    }
+	}
+	
+	render() {
+		return (
+			<React.Fragment>
+			<body id="page-top">
+
+<nav className="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
+  <a className="navbar-brand js-scroll-trigger" href="#page-top">
+    <span className="d-block d-lg-none">Clarence Taylor</span>
+    <span className="d-none d-lg-block">
+    <i class="fab fa-bitcoin fa-2x"></i>
+          {/* <img className="img-fluid img-profile rounded-circle mx-auto mb-2" src="img/profile.jpg" alt=""></img> */}
+    </span>
+  </a>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul className="navbar-nav">
+      <li className="nav-item">
+        <a className="nav-link js-scroll-trigger" href="#about">About</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link js-scroll-trigger" href="#experience">r/CryptoCurrency</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link js-scroll-trigger" href="#education">/biz/</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link js-scroll-trigger" href="#skills">ccn.com</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link js-scroll-trigger" href="#interests">coindesk.com</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link js-scroll-trigger" href="#awards">coinmonks.com</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+<div className="container-fluid p-0">
+
+  <section className="resume-section p-3 p-lg-5 d-flex d-column" id="about">
+    <div className="my-auto">
+      <h1 className="mb-0">Crypto
+        <span className="text-primary">Trends</span>
+      </h1>
+      <div className="subheading mb-5">
+      <p className="lead text-bold mb-5">Click a site on the left to start.</p>
+        {/* <a href="mailto:placeholder@email.com">placeholder@email.com</a> */}
+      </div>
+      <p className="lead mb-5">By finding the ratio of BTC vs altcoin (ETH,LTC,XMR) mentions per site and comparing this with BTC volume strength over time with can find the correlation coefficient r values and identify which site most accurately reflects or anticipates trends.</p>
+      <div className="social-icons">
+        <a href="#">
+          <i className="fab fa-linkedin-in"></i>
+        </a>
+        <a href="#">
+          <i className="fab fa-github"></i>
+        </a>
+        <a href="#">
+          <i className="fab fa-twitter"></i>
+        </a>
+        <a href="#">
+          <i className="fab fa-facebook-f"></i>
+        </a>
+      </div>
+    </div>
+  </section>
+
+  <hr className="m-0"/>
+
+  <section className="resume-section p-3 p-lg-5 d-flex flex-column" id="experience">
+    <div className="my-auto">
+      <h2 className="mb-5">r/cryptocurrency <span className="d-block text-primary">78%</span></h2>
+
+            <ScrapeTrendsHighChart/>
+
+    </div>
+  </section>
+
+  <hr className="m-0"/>
+
+  <section className="resume-section p-3 p-lg-5 d-flex flex-column" id="education">
+    <div className="my-auto">
+      <h2 className="mb-5">/biz/ <span className="d-block text-primary">78%</span></h2>
+
+       <ScrapeTrendsHighChart/>
+
+    </div>
+  </section>
+
+  <hr className="m-0"/>
+
+  <section className="resume-section p-3 p-lg-5 d-flex flex-column" id="skills">
+    <div className="my-auto">
+      <h2 className="mb-5">ccn.com <span className="d-block text-primary">78%</span> </h2>
+
+            <ScrapeTrendsHighChart/>
+
+    </div>
+  </section>
+
+  <hr className="m-0"/>
+
+  <section className="resume-section p-3 p-lg-5 d-flex flex-column" id="interests">
+    <div className="my-auto">
+      <h2 className="mb-5">coindesk.com  <span className="d-block text-primary">78%</span> </h2>
+      <ScrapeTrendsHighChart/>
+
+    </div>
+  </section>
+
+  <hr className="m-0"/> 
+
+  <section className="resume-section p-3 p-lg-5 d-flex flex-column" id="awards">
+    <div className="my-auto">
+      <h2 className="mb-5">coinmonks.com      <span className="d-block text-primary">78%</span> </h2>
+      <ScrapeTrendsHighChart/>
+
+    </div>
+  </section>
+
+</div>
+
+<script src="vendor/jquery/jquery.min.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+<script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+<script src="js/resume.js"></script>
+
+<script src="/css/resume.css"></script>
+
+			</body>
+			</React.Fragment>
+		)
+	}
+}
+ export default MainPage; 
---
+++ src/ScrapeTrends.js
@@ -0,0 +1,242 @@
+import React from "react";
+// import ReactDOM from "react-dom";
+// import * as scrapeService from "../services/scrape.service";
+import ReactHighcharts from 'react-highcharts';
+// import { onChange, showFormErrors } from "../../helpers/validation.helper";
+let param = "";
+
+
+class ScrapeTrendsHighChart extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            startDatelimit: null,
+            endDatelimit: null,
+            config: null,
+            initialRender: false,
+            formData: {
+                start: null,
+                end: null
+            },
+            errors: {},
+            message: ''
+
+        }
+        // this.datePickerOnChange = onChange.bind(this);
+    }
+
+    // datePickerSet = event => {
+    //     if (!showFormErrors.call(this)) {
+    //         return;
+    //     }
+    //     if (this.state.formData.start) {
+    //         param = "?start=" + this.state.formData.start;
+    //     }
+    //     if (this.state.formData.end && !this.state.formData.start) {
+    //         param = param + "?end=" + this.state.formData.end;
+    //     } else if (this.state.formData.end) {
+    //         param = param + "&end=" + this.state.formData.end;
+    //     }
+    //     this.metricsCall(param)
+    //     param = "";
+    // }
+
+    // scrapeCall(param){
+    //     scrapeService.getScrape(param)
+    //         .then(response => {
+    //             if (!this.state.initialRender) {
+    //                 this.allDataEnd = response[response.length - 1]._id;
+    //             }
+    //             this.setState({
+    //                 initialRender: true,
+    //                 config: {
+    //                     title: {
+    //                         text: ''
+    //                     },
+
+    //                     chart: {
+    //                         height: 480
+    //                     },
+
+    //                     subtitle: {
+    //                         // text: 'Source: stats'
+    //                     },
+
+    //                     xAxis: {
+    //                         categories: response.map(item => item._id)
+    //                     },
+
+    //                     yAxis: {
+    //                         title: {
+    //                             text: 'Amount of Requests'
+    //                         }
+    //                     },
+    //                     legend: {
+    //                         layout: 'vertical',
+    //                         align: 'right',
+    //                         verticalAlign: 'middle'
+    //                     },
+
+    //                     plotOptions: {
+    //                         series: {
+    //                             label: {
+    //                                 connectorAllowed: false
+    //                             }
+    //                         }
+    //                     },
+
+    //                     series: [{
+    //                         name: 'Request Created',
+    //                         data: response.map(item => {
+    //                             return item.count
+    //                         })
+    //                     }],
+
+    //                     responsive: {
+    //                         rules: [{
+    //                             condition: {
+    //                                 maxWidth: 500
+    //                             },
+    //                             chartOptions: {
+    //                                 legend: {
+    //                                     layout: 'horizontal',
+    //                                     align: 'center',
+    //                                     verticalAlign: 'bottom'
+    //                                 }
+    //                             }
+    //                         }]
+    //                     }
+    //                 }
+    //             })
+    //         })
+    // }
+
+    componentDidMount() {
+        // this.scrapeCall(param);
+
+                    this.setState({
+                    initialRender: true,
+                    config: {
+                        title: {
+                            text: ''
+                        },
+
+                        chart: {
+                            height: 280
+                        },
+
+                        subtitle: {
+                            // text: 'Source: stats'
+                        },
+
+                        xAxis: {
+                            // categories: response.map(item => item._id)
+                            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+                            
+                        },
+                        series: [{
+                            data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 295.6, 454.4]
+                          }],
+
+                        yAxis: {
+
+                            title: {
+                                text: 'Amount of Mentions'
+                            }
+                        },
+                        legend: {
+                            layout: 'vertical',
+                            align: 'right',
+                            verticalAlign: 'middle'
+                        },
+
+                        plotOptions: {
+                            series: {
+                                compare: 'percent',
+                                showInNavigator: true,
+                                label: {
+                                    connectorAllowed: false
+                                }
+                            }
+                        },
+
+                        series: [{
+                            name: 'Y Mentions',
+                            data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 295.6, 454.4],
+
+                            // data: response.map(item => {
+                            //     return item.count
+                            // })
+                        }],
+
+                        responsive: {
+                            rules: [{
+                                condition: {
+                                    maxWidth: 500
+                                },
+                                chartOptions: {
+                                    legend: {
+                                        layout: 'horizontal',
+                                        align: 'center',
+                                        verticalAlign: 'bottom'
+                                    }
+                                }
+                            }]
+                        }
+                    }
+                })
+
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                                        <span className="d-block d-lg-none" >78%</span> 
+
+                <div style={{ "padding": "15px", "background": "#fff", "text-align": "center", "color": "#333333", "font-size": "18px", "fill": "#333333", "font-family": "Lucida Grande, Lucida Sans Unicode, Arial, Helvetica, sans-serif" }}>
+                    <div>Mentions over time</div>
+                    <div style={{"font-size": "12px", "color": "#666", "margin-top":"8px"}}>Source: Binance </div>
+                </div>
+                {this.state.config &&
+
+                    <div>
+                        <div className="col-md-12" style={{ "backgroundColor": "white", "padding": "5px" }}>
+                            <center><form style={{ "display": "table", "width": "100%" }}>
+                                <div id="startLabel" style={{ "display": "table-cell", "width": "30%" }} className={"form-group " + (this.state.errors.start ? "has-error" : "")}>
+                                    <div className="rounded no-overflow">
+                                        <label htmlFor="start">Start</label>
+                                        &nbsp;
+                                        <input type="date" className="rounded" id="start" name="start" ref="start"
+                                            max={new Date().toISOString().split("T")[0]}
+                                            //max set to today for now or else year will be able to contain 5 digits
+                                            onChange={this.datePickerOnChange}
+                                        />
+                                    </div>
+                                    <div className="asterisk">{this.state.errors.start}</div>
+                                </div>
+
+                                <div id="endLabel" style={{ "display": "table-cell", "width": "30%" }} className={"form-group " + (this.state.errors.end ? "has-error" : "")}>
+                                    <div className="rounded no-overflow">
+                                        <label htmlFor="end">End</label>
+                                        &nbsp;
+                                        <input type="date" className="rounded" id="end" name="end" ref="end"
+                                            max={new Date().toISOString().split("T")[0]}
+                                            onChange={this.datePickerOnChange} />
+                                    </div>
+                                    <div className="asterisk">{this.state.errors.end}</div>
+                                </div>
+                                <div style={{ "display": "table-cell", "width": "30%" }}><button type="button" className="btn btn-outline-dark btn-xs btn-slidedown" onClick={this.datePickerSet}>Set Range</button></div>
+                            </form>
+                            </center>
+                        </div>
+
+                        <ReactHighcharts style={{ "position": "absolute", "top": 0, "left": 0 }} config={this.state.config}></ReactHighcharts>
+                        
+                    </div>}
+
+            </React.Fragment>
+        )
+    }
+}
+
+export default ScrapeTrendsHighChart;
