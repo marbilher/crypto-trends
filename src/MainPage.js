5475db672999bedba84e22e959fa5e2fb30edec7
Marlon B Hernandez <marlonbilbaohernandez@gmail.com>
2018-09-18 11:13:45 -0700

    fix navbar toggle for cell

--- src/MainPage.js
+++ src/MainPage.js
@@ -20,7 +20,7 @@ import ScrapeTrendsHighChart from './ScrapeTrends.js'
 
 <nav className="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
   <a className="navbar-brand js-scroll-trigger" href="#page-top">
-    <span className="d-block d-lg-none">Clarence Taylor</span>
+    <span className="d-block d-lg-none">CryptoTrends</span>
     <span className="d-none d-lg-block">
     <i class="fab fa-bitcoin fa-2x"></i>
           {/* <img className="img-fluid img-profile rounded-circle mx-auto mb-2" src="img/profile.jpg" alt=""></img> */}
